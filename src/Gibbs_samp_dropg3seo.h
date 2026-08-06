f9acd6cba40eeec5d2b287cf95da8f731cacc8d2
shilab-ecnu <xjshi@fem.ecnu.edu.cn>
2026-08-06 20:08:02 +0800

    Add files via upload

---
+++ src/Gibbs_samp_dropg3seo.h
@@ -0,0 +1,42 @@
+#ifndef Gibbs_samp_hpp
+#define Gibbs_samp_hpp
+
+#include <RcppArmadillo.h>
+#include <Rcpp.h>
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+
+using namespace Rcpp;
+using namespace arma;
+using namespace std;
+
+// [[Rcpp::depends(RcppArmadillo)]]
+
+
+
+struct ObjGibbs_dropg3{
+  vec gamma1;
+  vec beta2;
+  vec gamma3;
+  vec Beta1res;
+  vec Beta4res;
+  vec Sg12Res;
+  vec Sg22Res;
+  vec Sg32Res;
+};
+
+ObjGibbs_dropg3 MRGibbs_dropg3(arma::vec &gammah1, arma::vec &Gammah1, arma::vec &Gammah3, 
+                               arma::vec &se1, arma::vec &se3, arma::vec &se4, arma::mat &R, double &rho_1,
+                               int maxIter, int burnin, int thin);
+
+
+
+
+
+
+
+
+
+#endif /* GibbsAlpGamEta_ptr_hpp */
