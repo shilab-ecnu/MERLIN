c7d5ce6002e88a82be5073e859a8fbe1f3b91b16
shilab-ecnu <xjshi@fem.ecnu.edu.cn>
2026-08-19 16:45:52 +0800

    Update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -269,7 +269,7 @@ BEGIN_RCPP
 END_RCPP
 }
 // MRGEI_Gamseo_fixb1
-List MRGEI_Gamseo_fixb1(arma::vec& gammah1, arma::vec& gammah3, arma::vec& Gammah1, arma::vec& se1, arma::vec& se2, arma::vec& se3, arma::mat& R, double& rho, double& b1, int maxIter, int burnin, int thin);
+List MRGEI_Gamseo(arma::vec& gammah1, arma::vec& gammah3, arma::vec& Gammah1, arma::vec& se1, arma::vec& se2, arma::vec& se3, arma::mat& R, double& rho, int maxIter, int burnin, int thin);
 RcppExport SEXP _MERLIN_MRGEI_Gamseo_fixb1(SEXP gammah1SEXP, SEXP gammah3SEXP, SEXP Gammah1SEXP, SEXP se1SEXP, SEXP se2SEXP, SEXP se3SEXP, SEXP RSEXP, SEXP rhoSEXP, SEXP b1SEXP, SEXP maxIterSEXP, SEXP burninSEXP, SEXP thinSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
@@ -282,11 +282,10 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< arma::vec& >::type se3(se3SEXP);
     Rcpp::traits::input_parameter< arma::mat& >::type R(RSEXP);
     Rcpp::traits::input_parameter< double& >::type rho(rhoSEXP);
-    Rcpp::traits::input_parameter< double& >::type b1(b1SEXP);
     Rcpp::traits::input_parameter< int >::type maxIter(maxIterSEXP);
     Rcpp::traits::input_parameter< int >::type burnin(burninSEXP);
     Rcpp::traits::input_parameter< int >::type thin(thinSEXP);
-    rcpp_result_gen = Rcpp::wrap(MRGEI_Gamseo_fixb1(gammah1, gammah3, Gammah1, se1, se2, se3, R, rho, b1, maxIter, burnin, thin));
+    rcpp_result_gen = Rcpp::wrap(MRGEI_Gamseo(gammah1, gammah3, Gammah1, se1, se2, se3, R, rho, maxIter, burnin, thin));
     return rcpp_result_gen;
 END_RCPP
 }
@@ -497,7 +496,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"_MERLIN_MRGEI_Gam3seo", (DL_FUNC) &_MERLIN_MRGEI_Gam3seo, 14},
     {"_MERLIN_MRGEI_Gam3seo_addE2", (DL_FUNC) &_MERLIN_MRGEI_Gam3seo_addE2, 14},
     {"_MERLIN_MRGEI_Gam3seo_binary", (DL_FUNC) &_MERLIN_MRGEI_Gam3seo_binary, 15},
-    {"_MERLIN_MRGEI_Gamseo_fixb1", (DL_FUNC) &_MERLIN_MRGEI_Gamseo_fixb1, 12},
+    {"_MERLIN_MRGEI_Gamseo", (DL_FUNC) &_MERLIN_MRGEI_Gamseo_fixb1, 11},
     {"_MERLIN_getLineNum", (DL_FUNC) &_MERLIN_getLineNum, 1},
     {"_MERLIN_ReadSNPinfo", (DL_FUNC) &_MERLIN_ReadSNPinfo, 8},
     {"_MERLIN_Read_summarystat", (DL_FUNC) &_MERLIN_Read_summarystat, 10},
