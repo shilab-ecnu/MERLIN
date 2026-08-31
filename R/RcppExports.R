bbedd3bdf71e515d7fbe9885ed0cb40cab88b63f
shilab-ecnu <xjshi@fem.ecnu.edu.cn>
2026-08-31 09:46:38 +0800

    Update RcppExports.R

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -53,16 +53,9 @@ Cal_block_Rvec <- function(bp, chr, avbIndex, block_file, stringname3, coreNum, 
     .Call(`_MERLIN_Cal_block_Rvec`, bp, chr, avbIndex, block_file, stringname3, coreNum, lam)
 }
 
-MRGEI_Gam3seo <- function(gammah1, gammah3, Gammah1, Gammah3, se1, se2, se3, se4, R, rho_1, rho_2, maxIter, burnin, thin) {
-    .Call(`_MERLIN_MRGEI_Gam3seo`, gammah1, gammah3, Gammah1, Gammah3, se1, se2, se3, se4, R, rho_1, rho_2, maxIter, burnin, thin)
-}
 
-MRGEI_Gam3seo_addE2 <- function(gammah1, gammah3, Gammah1, Gammah3, se1, se2, se3, se4, R, rho_1, rho_2, maxIter, burnin, thin) {
-    .Call(`_MERLIN_MRGEI_Gam3seo_addE2`, gammah1, gammah3, Gammah1, Gammah3, se1, se2, se3, se4, R, rho_1, rho_2, maxIter, burnin, thin)
-}
-
-MRGEI_Gam3seo_binary <- function(gammah1, gammah3, Gammah1, Gammah3, se1, se2, se3, se4, R, rho_1, rho_2, p1, maxIter, burnin, thin) {
-    .Call(`_MERLIN_MRGEI_Gam3seo_binary`, gammah1, gammah3, Gammah1, Gammah3, se1, se2, se3, se4, R, rho_1, rho_2, p1, maxIter, burnin, thin)
+MRGEI_Gam3seo_3to1 <- function(gammah1, gammah3, Gammah1, Gammah3, se1, se2, se3, se4, R, rho_1, rho_2, p1, maxIter, burnin, thin) {
+    .Call(`_MERLIN_MRGEI_Gam3seo_3to1`, gammah1, gammah3, Gammah1, Gammah3, se1, se2, se3, se4, R, rho_1, rho_2, p1, maxIter, burnin, thin)
 }
 
 MRGEI_Gamseo <- function(gammah1, gammah3, Gammah1, se1, se2, se3, R, rho, maxIter, burnin, thin) {
